90b5b6f5d57bd8256f1047284a0929e49b5da773
Akzhol <imangazin286@gmail.com>
2023-08-23 03:54:00 +0600

    added spm

---
+++ Tests/FlagPhoneNumberTests/FlagPhoneNumberTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FlagPhoneNumber
+
+final class FlagPhoneNumberTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FlagPhoneNumber().text, "Hello, World!")
+    }
+}
